e445ea7bcb4ecf090d0e909480b5bb38feac2f57
Masahiro Okazaki <okapee.masapiro@gmail.com>
2020-02-16 21:13:29 +0900

    [add]Card Widgetと遷移の追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ void main() {
   runApp(new MaterialApp(
     home: new MyApp(), // becomes the route named '/'
     routes: <String, WidgetBuilder>{
-      '/a': (BuildContext context) => new MyPage(),
-      '/b': (BuildContext context) => new MyPage(),
-      '/c': (BuildContext context) => new MyPage(),
+      '/book_detail': (BuildContext context) => new BookDetail(),
     },
   ));
 }
@@ -16,35 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ブックレンタルアプリ',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'ブックレンタルアプリ'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -53,38 +33,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // TODO: グループIDで登録されている本の一覧をDBから取得し、そのレコード数をCardの要素数とする
+  static int length = 30;
+  var cardlist = List.generate(length, (index) => index);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: GridView.count(
         crossAxisCount: 2,
         padding: EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
         children: <Widget>[
           Card(
             clipBehavior: Clip.antiAlias,
@@ -103,61 +65,90 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: <Widget>[
                       Text('Title'),
                       SizedBox(height: 8.0),
-                      Text('Secondry Text'),
+                      Text('First Text'),
                     ],
                   ),
                 ),
               ],
             ),
           ),
-          Card(
-            child: new InkWell(
-              onTap: () {
-                Navigator.of(context).pushNamed('/b');
-              },
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    AspectRatio(
-                      aspectRatio: 18.0 / 11.0,
-                      child: Image.asset(
-                          'assets/f_f_object_174_s128_f_object_174_1bg.png'),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pushNamed('/book_detail');
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset(
+                        'assets/f_f_object_174_s128_f_object_174_0bg.png.png'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondry Text'),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('Title'),
-                          SizedBox(height: 8.0),
-                          Text('Secondry Text'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
+//          Card(
+//            child: new InkWell(
+////              onTap: () {
+////                Navigator.of(context).pushNamed('/b');
+////              },
+//              child: Container(
+//                child: Column(
+//                  crossAxisAlignment: CrossAxisAlignment.start,
+//                  children: <Widget>[
+//                    AspectRatio(
+//                      aspectRatio: 18.0 / 11.0,
+//                      child: Image.asset(
+//                          'assets/f_f_object_174_s128_f_object_174_1bg.png'),
+//                    ),
+//                    Padding(
+//                      padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+//                      child: Column(
+//                        crossAxisAlignment: CrossAxisAlignment.start,
+//                        children: <Widget>[
+//                          Text('Title'),
+//                          SizedBox(height: 8.0),
+//                          Text('Secondry Text'),
+//                        ],
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+//            ),
+//          ),
           Card()
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class MyPage extends StatelessWidget {
+class BookDetail extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _BookDetailState createState() => new _BookDetailState();
+}
+
+class _BookDetailState extends State<BookDetail> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '2nd Page',
+    return new Scaffold(
+      appBar: AppBar(title: Text('BookDetail')),
+      body: Container(),
     );
   }
 }
