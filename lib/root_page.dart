05edd1fa739309df66b3bc294123e0b4c1aace2c
Masahiro Okazaki <okapee.masapiro@gmail.com>
2020-02-23 22:35:55 +0900

    [add]ログイン関連ファイルを追加（ページ遷移未実装）

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home_page.dart';
-import 'package:flutter_app/main.dart';
 
 import 'auth.dart';
 import 'home_page.dart';
